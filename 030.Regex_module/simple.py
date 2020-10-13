4cc9d554bb2d9a7a3e9d6c371ae357cd6d9aaba9
Adrija Bhandari <adrija.bhandari@gmail.com>
2020-10-12 20:50:54 -0700

    - completed lesson 30 : Regex_module

---
+++ 030.Regex_module/simple.py
@@ -0,0 +1,46 @@
+import re
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \\ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+123*555-1234
+800-555-1234
+900-555-1234
+
+bat
+cat
+mat
+sat
+hat
+pat
+eat
+fat
+rat
+tat
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+pattern = re.compile(r'start', re.I)
+
+matches = pattern.search(sentence)
+
+print(matches)
